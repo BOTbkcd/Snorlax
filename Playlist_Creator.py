c3f5d2a85263d5a47c97d56a635c1e4dc8015a3d
G@rv!t <f2015148p@alumni.bits-pilani.ac.in>
2023-08-14 01:58:58 +0530

    Added title sort

--- Playlist_Creator.py
+++ Playlist_Creator.py
@@ -10,5 +10,6 @@ for playlist in playlists:
       playlist_title = os.path.basename(playlist)
       playlist_content = [("Playlists/" + playlist_title + "/" + f.name ) for f in os.scandir(playlist) if f.is_file()] 
 
+      playlist_content.sort()
       with open(PLAYLIST_DESTINATION_PATH + playlist_title + ".m3u", "w") as m3u_file:
         m3u_file.write('\n'.join(playlist_content))
